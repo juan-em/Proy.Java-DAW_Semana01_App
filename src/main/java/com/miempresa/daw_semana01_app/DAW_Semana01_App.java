cd57c17e306a33b78fc0f6512bcd5c4dd03ee598
juan-em <9jescobarm6@gmail.com>
2023-08-11 10:00:23 -0500

    commit

---
+++ src/main/java/com/miempresa/daw_semana01_app/DAW_Semana01_App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.miempresa.daw_semana01_app;
+
+/**
+ *
+ * @author jhons
+ */
+public class DAW_Semana01_App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
